ac883cb7b651381d64fcfb73bf9786c8a8d22fa1
dfontana <dmz.sfx@gmail.com>
2016-10-17 18:58:50 -0400

    added sass compilation and style modules

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var stylemod = require('gulp-style-modules');
+var path = require('path');
 
-//Compiles sass
+
+//Compiles sass to css
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 });
 
+// Wrap css files into style modues
+gulp.task("modularize-styles",['styles'], function() {
+    gulp.src("./css/**/*.css")
+        .pipe(stylemod({
+            // All files will be named 'styles.html'
+            filename: function(file) {
+                return path.basename(file.path, path.extname(file.path)) + "-module";
+            },
+            
+            // Use '-css' suffix instead of '-styles' for module ids
+            moduleId: function(file) {
+                return path.basename(file.path, path.extname(file.path)) + "-css";
+            }
+        }))
+        .pipe(gulp.dest("./src"));
+});
+
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('sass/**/*.scss',['modularize-styles']);
 });
